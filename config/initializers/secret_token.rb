e38acda28d6ccd4f98929428fd75363ecd3b83bd
Juliana Lucena <julianalucenaa@gmail.com>
2012-01-04 15:01:21 -0300

    Rails project created.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Donefy::Application.config.secret_token = '3dc634086456921a8ed620e9d3cd0f67d2b83c01716bb245c101653348024927eaa2b48591005f776bf232d8afff96c63d99c2f12006184c832fcaf0db37475b'
